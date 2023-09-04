ed02be851e7b1f664b374846f491d8d4611936ec
AoiTuNa <bestspark123@naver.com>
2023-09-04 17:38:54 +0900

    sum클래스 추가 및 메인 함수 수정

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main{
+    public static void main(String[] args) {
+        sum = new Sum();
+        int x = 10;
+        int y = 20;
+        sum.add(x,y);
+        x.sub(y);
+        x.mul(y);
+        x.div(y);
+    }
+}
---
+++ Sum.java
@@ -0,0 +1,13 @@
+public class Sum {
+    int x;
+    int y;
+    Sum(){
+
+    }
+    public int add(int num1, int num2){
+        this.x = num1;
+        this.y = num2;
+        return x + y;
+
+    }
+}
