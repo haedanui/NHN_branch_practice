9c95eb52f5b20ec0ed221c333dee659b415f80ee
haedanui <rhs9705kr@naver.com>
2023-09-04 17:36:28 +0900

    div 추가 및 메인 수정

--- Main.java
+++ Main.java
@@ -2,9 +2,9 @@ class Main{
     public static void main(String[] args) {
         int x = 10;
         int y = 20;
-        add(int x, int y);
-        sub(int x, int y);
-        mul(int x, int y);
-        sub(int x, int y);
+        add(x, y);
+        sub(x, y);
+        mul(x, y);
+        div.div(x, y);
     }
 }
