2fa6834b0dcd9b8479d7aee7f76ce9ad22e55fe3
AoiTuNa <bestspark123@naver.com>
2023-09-04 17:45:37 +0900

    돌아가게 수정한 더하기

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 class Main{
+    
     public static void main(String[] args) {
-        sum = new Sum();
         int x = 10;
         int y = 20;
-        sum.add(x,y);
+        add.add(x,y);
         x.sub(y);
         x.mul(y);
         x.div(y);
